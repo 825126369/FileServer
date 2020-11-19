3fe0ce2512a41b3594b4e188acce8df1c0c681eb
xuke2020 <825126369@qq.com>
2020-11-19 12:20:36 +0800

    no message

--- WebServer/Startup.cs
+++ WebServer/Startup.cs
@@ -49,25 +49,24 @@ namespace WebServer
             }
 
             app.UseHttpsRedirection();
-            //app.UseStaticFiles();
+            app.UseStaticFiles();
 
-            //app.UseFileServer(new FileServerOptions()
-            //{
-            //    FileProvider = new PhysicalFileProvider(
-            //                Path.Combine(Directory.GetCurrentDirectory(), @"StaticFiles")),
-            //    RequestPath = new PathString("/StaticFiles"),
-            //    EnableDirectoryBrowsing = true
-            //});
-            
-            var dir = new DirectoryBrowserOptions();
-            dir.FileProvider = new PhysicalFileProvider(@"/Users/xuke2020/Documents/Project/FileServer/WebServer/StaticFiles/");
-            app.UseDirectoryBrowser(dir);
+            FileServerOptions fileServerOptions = new FileServerOptions();
+            fileServerOptions.EnableDefaultFiles = true;
+            fileServerOptions.EnableDirectoryBrowsing = true;
+            fileServerOptions.FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"StaticFiles"));
+            fileServerOptions.StaticFileOptions.ServeUnknownFileTypes = true;
+            app.UseFileServer(fileServerOptions);
 
-            //更改默认文件夹 (StaticFileOptions方法)
-            var staticfile = new StaticFileOptions();
-            staticfile.FileProvider = new PhysicalFileProvider(@"/Users/xuke2020/Documents/Project/FileServer/WebServer/StaticFiles/");//指定目录，这里指C盘，也可以是其他目录
-            app.UseStaticFiles(staticfile);//使用默认文件夹wwwroot //手动设置MIME Type,或者设置一个默认值， 以解决某些文件MIME Type文件识别不到，出现404错误
-            staticfile.ServeUnknownFileTypes = true;
+            //var dir = new DirectoryBrowserOptions();
+            //dir.FileProvider = new PhysicalFileProvider("../StaticFiles/");
+            //app.UseDirectoryBrowser(dir);
+
+            ////更改默认文件夹 (StaticFileOptions方法)
+            //var staticfile = new StaticFileOptions();
+            //staticfile.FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"StaticFiles"));
+            //app.UseStaticFiles(staticfile);
+            //staticfile.ServeUnknownFileTypes = true;
             
             //staticfile.DefaultContentType = "application/octet-stream";
             //var provider = new FileExtensionContentTypeProvider();//使用一组默认映射创建新的提供程序
@@ -76,7 +75,7 @@ namespace WebServer
 
             app.UseRouting();
 
-            //app.UseAuthorization();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
